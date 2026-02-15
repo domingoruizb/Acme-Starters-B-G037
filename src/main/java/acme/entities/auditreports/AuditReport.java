9a96aaaabba6c7af458f6c46f1459b525078c004
aurbinat <alejandroubty@gmail.com>
2026-02-15 18:07:10 +0100

    task/implement-model-c05

---
+++ src/main/java/acme/entities/auditreports/AuditReport.java
@@ -0,0 +1,90 @@
+
+package acme.entities.auditreports;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoment.Constraint;
+import acme.client.components.validation.ValidUrl;
+import acme.realms.Auditor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AuditReport extends AbstractEntity {
+
+	// Serialisation version
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+
+	@Mandatory
+	// @ValidTicker
+	@Column(unique = true)
+	private String				ticker;
+
+	@Mandatory
+	// @ValidHeader
+	@Column
+	private String				name;
+
+	@Mandatory
+	// @ValidText
+	@Column
+	private String				description;
+
+	@Mandatory
+	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
+	@Column
+	private Moment				startMoment;
+
+	@Mandatory
+	@ValidMoment(constraint = Constraint.ENFORCE_FUTURE)
+	@Column
+	private Moment				endMoment;
+
+	@Optional
+	@ValidUrl
+	@Column
+	private String				moreInfo;
+
+	@Mandatory
+	@Valid
+	@Column
+	private Boolean				draftMode;
+
+	// Derived attributes
+	/*
+	 * @Mandatory
+	 * 
+	 * @Valid
+	 * 
+	 * @Transient
+	 * private Double getMonthsActive() {
+	 * }
+	 * 
+	 * @Mandatory
+	 * 
+	 * @ValidNumber(min = 0)
+	 * 
+	 * @Transient
+	 * private Integer getHours() {
+	 * }
+	 */
+
+	// Relationships
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Auditor				auditor;
+}
