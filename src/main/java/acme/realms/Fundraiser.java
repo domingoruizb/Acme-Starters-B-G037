18e11cffe9ce7141f937ce31690f75c341b12a8e
LMX8564 <eleferrei@alum.us.es>
2026-02-16 11:04:42 +0100

    Abstract entity changed

--- src/main/java/acme/realms/Fundraiser.java
+++ src/main/java/acme/realms/Fundraiser.java
@@ -5,7 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.Valid;
 
-import acme.client.components.basis.AbstractEntity;
+import acme.client.components.basis.AbstractRole;
 import acme.client.components.validation.Mandatory;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,7 +13,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Fundraiser extends AbstractEntity {
+public class Fundraiser extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
 
