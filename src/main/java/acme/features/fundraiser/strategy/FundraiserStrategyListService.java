1c8a8753220161ebf901ca50db0a5a58c7241107
LMX8564 <eleferrei@alum.us.es>
2026-03-15 17:54:03 +0100

    Strategy CRUD

---
+++ src/main/java/acme/features/fundraiser/strategy/FundraiserStrategyListService.java
@@ -0,0 +1,39 @@
+
+package acme.features.fundraiser.strategy;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.services.AbstractService;
+import acme.entities.strategy.Strategy;
+import acme.realms.Fundraiser;
+
+@Service
+public class FundraiserStrategyListService extends AbstractService<Fundraiser, Strategy> {
+
+	@Autowired
+	private FundraiserStrategyRepository	repository;
+
+	private Collection<Strategy>			strategies;
+
+
+	@Override
+	public void authorise() {
+		super.setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		int fundraiserId;
+
+		fundraiserId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		this.strategies = this.repository.findStrategyByFundraiserId(fundraiserId);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObjects(this.strategies, "ticker", "name", "startMoment", "endMoment", "draftMode");
+	}
+}
